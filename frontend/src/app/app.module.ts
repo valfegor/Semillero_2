37793d2a3e5268f16e40b60e1135d1dc1fc71aa2
Anthony Ahumada <locopala99@gmail.com>
2021-08-24 18:11:44 +0200

    More js

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,22 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './home/header/header.component';
 import { LoginComponent } from './home/login/login.component';
 import { RegisterComponent } from './home/register/register.component';
 import { FooterComponent } from './home/footer/footer.component';
-import { ListTaskComponent } from './board/list-task/list-task.component';
-import { ListUserComponent } from './admin/list-user/list-user.component';
-import { RegisterUserComponent } from './admin/register-user/register-user.component';
-import { UpdateUserComponent } from './admin/update-user/update-user.component';
-import { RegisterRoleComponent } from './admin/register-role/register-role.component';
-import { UpdateRolComponent } from './admin/update-rol/update-rol.component';
-
-import { UserService } from "./services/user.service";
-import { RoleService } from "./services/role.service";
-import { BoardService } from "./services/board.service";
-import {TokenInterceptorService  } from "./services/token-interceptor.service";
 
 @NgModule({
   declarations: [
@@ -24,18 +14,13 @@ import {TokenInterceptorService  } from "./services/token-interceptor.service";
     HeaderComponent,
     LoginComponent,
     RegisterComponent,
-    FooterComponent,
-    ListTaskComponent,
-    ListUserComponent,
-    RegisterUserComponent,
-    UpdateUserComponent,
-    RegisterRoleComponent,
-    UpdateRolComponent
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
-  providers: [UserService,RoleService,BoardService,TokenInterceptorService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
