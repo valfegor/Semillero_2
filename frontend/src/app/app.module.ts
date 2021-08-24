bf0b8bf8d8d0ee800b9ceea5441055054763a879
Anthony Ahumada <locopala99@gmail.com>
2021-08-24 14:23:29 +0200

    more js

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,6 +6,12 @@ import { HeaderComponent } from './home/header/header.component';
 import { LoginComponent } from './home/login/login.component';
 import { RegisterComponent } from './home/register/register.component';
 import { FooterComponent } from './home/footer/footer.component';
+import { ListTaskComponent } from './board/list-task/list-task.component';
+import { ListUserComponent } from './admin/list-user/list-user.component';
+import { RegisterUserComponent } from './admin/register-user/register-user.component';
+import { UpdateUserComponent } from './admin/update-user/update-user.component';
+import { RegisterRoleComponent } from './admin/register-role/register-role.component';
+import { UpdateRolComponent } from './admin/update-rol/update-rol.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +19,13 @@ import { FooterComponent } from './home/footer/footer.component';
     HeaderComponent,
     LoginComponent,
     RegisterComponent,
-    FooterComponent
+    FooterComponent,
+    ListTaskComponent,
+    ListUserComponent,
+    RegisterUserComponent,
+    UpdateUserComponent,
+    RegisterRoleComponent,
+    UpdateRolComponent
   ],
   imports: [
     BrowserModule
