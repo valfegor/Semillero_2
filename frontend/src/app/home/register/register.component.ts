c2b471aa4645bcff125bc1ae4dcd25da449f5e71
Anthony Ahumada <locopala99@gmail.com>
2021-08-25 11:14:23 +0200

    more js

--- frontend/src/app/home/register/register.component.ts
+++ frontend/src/app/home/register/register.component.ts
@@ -18,10 +18,37 @@ import {
 })
 export class RegisterComponent implements OnInit {
   //aqui van las variables globales
-  
+  /*
+  public = cualquier archivo que invoque
+  private = solo este archivo va a utilizar esta variable o este metodo
+  si no se agrega nada las variables deben ser publicas
+  */
+  public message: string;
+  public horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  public VerticalPosition: MatSnackBarVerticalPosition = 'top';
   //son items que necesito que se construyan antes de que se arme el archivo , el constuctor amrama ,
-  constructor() {}
+  constructor(
+    private _userService: UserService,
+    private _router: Router,
+    private _snackbar: MatSnackBar
+  ) {
+    //antes de que se construya el archivo la inicializo
+    this.message = '';
+    //mensajehorizontal
+  }
 
   //significa que cuando se ejecute el archivo y las propiedades del archivo el codigo que este adentro se va a ejecutar cuando cargue el archivo , ejemplo que las tareas se listen apenas inicie sesion
   ngOnInit(): void {}
+
+  registerUser(){
+
+  }
+
+  openSnackBarSuccesfull(){
+
+  }
+
+  openSnackBarError(){
+    
+  }
 }
