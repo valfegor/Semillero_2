495b5e172e6ec25e27b2fb37319ea937b20f5956
Anthony Ahumada <locopala99@gmail.com>
2021-08-25 10:45:35 +0200

    more js

--- frontend/src/app/home/register/register.component.ts
+++ frontend/src/app/home/register/register.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+//import del servicio del usuario.
+import { UserService } from '../../services/user.service';
+//se importa el router por que se va a comunicar por medio de rutas.
+import { Router } from '@angular/router';
+//traemos el snapbar por que este nos va a mostrar ciertos mensajes
+//es una barra de mensajes
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  //aqui van las variables globales
+  
+  //son items que necesito que se construyan antes de que se arme el archivo , el constuctor amrama ,
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  //significa que cuando se ejecute el archivo y las propiedades del archivo el codigo que este adentro se va a ejecutar cuando cargue el archivo , ejemplo que las tareas se listen apenas inicie sesion
+  ngOnInit(): void {}
 }
