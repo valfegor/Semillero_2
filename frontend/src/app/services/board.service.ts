7a76835e5a8776f76ed5084da745c4d2ea36452f
Anthony Ahumada <locopala99@gmail.com>
2021-08-30 09:46:39 +0200

    more js

--- frontend/src/app/services/board.service.ts
+++ frontend/src/app/services/board.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import {    Router  } from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BoardService {
+  private env :string;
+  constructor(private _router: Router , private _http: HttpClient) { 
+    this.env = environment.APP_URL;
+  }
 
-  constructor() { }
+  saveTask(board:any){
+
+  }
+
+  listTask(){
+
+  }
+
+  updateTask(board:any){
+
+  }
+
+  deleteTask(board:any){
+    
+  }
 }
