b1d69b6a82b042f8b5e7249c4f9774f2ec473ce3
Anthony Ahumada <locopala99@gmail.com>
2021-08-30 08:23:10 +0200

    More js

--- frontend/src/app/services/token-interceptor.service.ts
+++ frontend/src/app/services/token-interceptor.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor  } from "@angular/common/http";
-import {  } from "module";
+import { UserService } from "./user.service";
 
 @Injectable({
   providedIn: 'root'
