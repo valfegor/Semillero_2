d9247e2044e4499a0a01585c1755bf851cae9ddd
Anthony Ahumada <locopala99@gmail.com>
2021-08-30 09:39:47 +0200

    more js

--- frontend/src/app/board/save-task/save-task.component.ts
+++ frontend/src/app/board/save-task/save-task.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+//import del servicio del usuario.
+import { BoardService } from '../../services/board.service';
+//se importa el router por que se va a comunicar por medio de rutas.
+import { Router } from '@angular/router';
+//traemos el snapbar por que este nos va a mostrar ciertos mensajes
+//es una barra de mensajes
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-save-task',
   templateUrl: './save-task.component.html',
-  styleUrls: ['./save-task.component.css']
+  styleUrls: ['./save-task.component.css'],
 })
 export class SaveTaskComponent implements OnInit {
+  public registerData: any;
+  public message: string;
+  public horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  public VerticalPosition: MatSnackBarVerticalPosition = 'top';
+  public duratioInseconds: number;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private _boardService: BoardService,
+    private _router: Router,
+    private _snackbar: MatSnackBar
+  ) {
+    this.message = "";
+    this.duratioInseconds = 2;
   }
 
+  ngOnInit(): void {}
+
+  saveTask(){
+    
+  }
+
+  openSnackBarSuccesfull() {
+    //this.messague = por que ha estado cambiando , {} = CONFIGURACIONES DE LA BARRA , propiedad de la duracion 
+    this._snackbar.open(this.message,'X',{
+      horizontalPosition:this.horizontalPosition,
+      verticalPosition:this.VerticalPosition,
+      duration:this.duratioInseconds*1000,
+      panelClass:['style-snackBarTrue']
+    });
+  }
+
+  openSnackBarError() {
+    this._snackbar.open(this.message,'X',{
+      horizontalPosition:this.horizontalPosition,
+      verticalPosition:this.VerticalPosition,
+      duration:this.duratioInseconds*1000,
+      panelClass:['style-snackBarFalse']
+    });
+  }
 }
