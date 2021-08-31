d0bf654768979efc1fc90f8a70c30a360f6ee4c1
Anthony Ahumada <locopala99@gmail.com>
2021-08-31 14:31:41 +0200

    more js

--- frontend/src/app/board/save-task/save-task.component.ts
+++ frontend/src/app/board/save-task/save-task.component.ts
@@ -22,36 +22,30 @@ export class SaveTaskComponent implements OnInit {
   public horizontalPosition: MatSnackBarHorizontalPosition = 'end';
   public VerticalPosition: MatSnackBarVerticalPosition = 'top';
   public duratioInseconds: number;
-  selectedFile:any;
+  selectedFile: any;
 
   constructor(
     private _boardService: BoardService,
     private _router: Router,
     private _snackbar: MatSnackBar
   ) {
-    this.message = "";
+    this.message = '';
     this.duratioInseconds = 2;
-    this.registerData = {}
+    this.registerData = {};
     //aqui se guarda la imagen desde la pagina
     this.selectedFile = null;
   }
 
   ngOnInit(): void {}
 
-  uploadImg(event:any){
-
+  uploadImg(event: any) {
+    //viene de un evento donde se hace target , en la posicion 0 por que solo va a subir 1 archivo
+    this.selectedFile = <File>event.target.files[0];
   }
 
-  saveTaskImg(){
-    
-  }
-
-  saveTask(){
-        //validamos que lleguen los datos
-    if (
-      !this.registerData.name ||
-      !this.registerData.description
-    ) {
+  saveTaskImg() {
+    //validamos que lleguen los datos
+    if (!this.registerData.name || !this.registerData.description) {
       console.log('Failed process:incomplete data');
       this.message = 'Failed process:imcomplete data';
       this.openSnackBarError();
@@ -62,7 +56,7 @@ export class SaveTaskComponent implements OnInit {
         (res) => {
           console.log(res);
           //guardamos en el local storage , para saber que hay un usuario registrado
-          
+
           //despues los redirecciona a guardar su primera tarea
           this._router.navigate(['/listTask']);
 
@@ -71,35 +65,66 @@ export class SaveTaskComponent implements OnInit {
           this.openSnackBarSuccesfull();
 
           //despues de todo debe quedar vacio de nuevo
-          this.registerData= {};
-
-          
+          this.registerData = {};
         },
-        (err)=>{
+        (err) => {
           console.log(err);
-          this.message=err.error;
+          this.message = err.error;
           this.openSnackBarError();
         }
       );
-    } 
+    }
+  }
+
+  saveTask() {
+    //validamos que lleguen los datos
+    if (!this.registerData.name || !this.registerData.description) {
+      console.log('Failed process:incomplete data');
+      this.message = 'Failed process:imcomplete data';
+      this.openSnackBarError();
+      this.registerData = {};
+    } else {
+      //servicio de usuario el subscribe es como el trycach
+      this._boardService.saveTask(this.registerData).subscribe(
+        (res) => {
+          console.log(res);
+          //guardamos en el local storage , para saber que hay un usuario registrado
+
+          //despues los redirecciona a guardar su primera tarea
+          this._router.navigate(['/listTask']);
+
+          this.message = 'Succesfull Task Registration';
+
+          this.openSnackBarSuccesfull();
+
+          //despues de todo debe quedar vacio de nuevo
+          this.registerData = {};
+        },
+        (err) => {
+          console.log(err);
+          this.message = err.error;
+          this.openSnackBarError();
+        }
+      );
+    }
   }
 
   openSnackBarSuccesfull() {
-    //this.messague = por que ha estado cambiando , {} = CONFIGURACIONES DE LA BARRA , propiedad de la duracion 
-    this._snackbar.open(this.message,'X',{
-      horizontalPosition:this.horizontalPosition,
-      verticalPosition:this.VerticalPosition,
-      duration:this.duratioInseconds*1000,
-      panelClass:['style-snackBarTrue']
+    //this.messague = por que ha estado cambiando , {} = CONFIGURACIONES DE LA BARRA , propiedad de la duracion
+    this._snackbar.open(this.message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.VerticalPosition,
+      duration: this.duratioInseconds * 1000,
+      panelClass: ['style-snackBarTrue'],
     });
   }
 
   openSnackBarError() {
-    this._snackbar.open(this.message,'X',{
-      horizontalPosition:this.horizontalPosition,
-      verticalPosition:this.VerticalPosition,
-      duration:this.duratioInseconds*1000,
-      panelClass:['style-snackBarFalse']
+    this._snackbar.open(this.message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.VerticalPosition,
+      duration: this.duratioInseconds * 1000,
+      panelClass: ['style-snackBarFalse'],
     });
   }
 }
